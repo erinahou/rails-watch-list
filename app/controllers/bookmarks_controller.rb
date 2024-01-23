bb3aed949ead2f1dc2da09bdd607c1c7b267d793
Erina Ho <erina.hou@gmail.com>
2024-01-23 18:57:25 +1100

    Got list of movies to appear, began delete method but need to sort out

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,19 +1,43 @@
 class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new]
+
   def new
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
-    raise
+    @bookmark = Bookmark.new(bookmark_params)
+    # List can only be found through the url hence the params
+    # Replace @bookmark.list_id = nil with the @list_id
+    @list = List.find(params[:list_id])
+    @bookmark.list_id = @list.id
+    # movie_id is in the params
+    # Replace @bookmark.movie_id with @movie.id
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @bookmark.movie_id = @movie.id
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  # def destroy
-  #   raise
-  # end
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    # @bookmark = @list.bookmarks.find(params[:id])
+    # raise
+    # @bookmark.destroy
+    redirect_to list_path(@list), status: :see_other
+  end
+
   private
 
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
   def bookmark_params
-    params.require(:bookmark).permit(:movie_id, :content)
+    params.require(:bookmark).permit(:movie, :comment)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create, :destroy]
+    resources :bookmarks, only: [:new, :create]
   end
+  resources :bookmarks, only: [:destroy]
 end
